fb534b30884079cea3d6b7eea806c9d5950f5234
sara <sara.zecha@gmail.com>
2021-07-04 10:02:17 +0200

    initial data structure

---
+++ GameOfLife/Model/Cell.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameOfLife.Model
+{
+    public class Cell : BindableBase
+    {
+        private bool _alive; 
+
+        public bool Alive
+        {
+            get => _alive;
+            set => SetProperty(ref _alive, value);
+        }
+
+        public DelegateCommand ButtonClickCommand { get; set; }
+
+        public Cell()
+        {
+            ButtonClickCommand = new DelegateCommand(() => Alive = !Alive);
+        }
+    }
+}
