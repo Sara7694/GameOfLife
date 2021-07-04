912ccf44fd60f0cd78e557c6618deb08d4edcf01
sara <sara.zecha@gmail.com>
2021-07-04 13:40:21 +0200

    add simulation with async Task

--- GameOfLife/ViewModels/MainViewModel.cs
+++ GameOfLife/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GameOfLife.ViewModels
 {
@@ -13,6 +14,9 @@ namespace GameOfLife.ViewModels
         CellGridCalculations calculations;
 
         private ObservableCollection<Cell> _cells;
+        private string _simualtionText; 
+
+        private bool SimulationRunning;
 
         public ObservableCollection<Cell> Cells 
         {
@@ -32,17 +36,36 @@ namespace GameOfLife.ViewModels
 
         public DelegateCommand RandomPopulationCommand { get; set; }
 
+        public String SimulationButtonText 
+        {
+            get => _simualtionText;
+            set => SetProperty(ref _simualtionText, value); 
+        }
+
         public MainViewModel()
         {
             
             calculations = new CellGridCalculations();
             Rows = 10;
-            Columns = 10; 
+            Columns = 10;
+            SimulationButtonText = " Start Simulation";
 
             Cells = calculations.GenerateGrid(Rows, Columns);
             ClearCommand = new DelegateCommand(() => calculations.ClearGrid(Cells));
             RandomPopulationCommand = new DelegateCommand(() => calculations.FillGridRandom(0.3, Cells));
             SimulateOneStepCommand = new DelegateCommand(() => Cells = calculations.SimulateOneStep(Cells, Rows, Columns));
+            StartSimulationCommand = new DelegateCommand(() => StartSimulationAsync());
+        }
+
+        private async Task StartSimulationAsync()
+        {
+            SimulationRunning = !SimulationRunning;
+            SimulationButtonText = SimulationRunning ? "Stop Simulation" : "Start Simulation";
+            while(SimulationRunning)
+            {
+                Cells = calculations.SimulateOneStep(Cells, Rows, Columns);
+                await Task.Delay(TimeSpan.FromMilliseconds(500)).ConfigureAwait(false);
+            }
         }
     }
 }
